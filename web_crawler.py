be7245e46131b55fe9280efbe61aff5f43b3748a
hshah11 <harshendrashah812@gmail.com>
2018-05-06 19:31:05 +0530

    Updates

--- web_crawler.py
+++ web_crawler.py
@@ -1,9 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-def trade_spider(max_pages):
-    page = 1
-    while page <= max_pages:
+def py_spider():
+    while True:
         url = 'https://docs.python.org/3/'
         source_code = requests.get(url)
         plain_text = source_code.text
@@ -11,8 +10,15 @@ def trade_spider(max_pages):
         for link in soup.findAll('a', {'class': 'biglink'}):
             href = "https://docs.python.org/3/" + link.get('href')
             title = link.string
-            print(href)
+            #print(href)
             print(title)
-        page += 1
+            get_single_item_data(href)
 
-trade_spider(1)
+def get_single_item_data(item_url):
+    source_code = requests.get(item_url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, "lxml")
+    for item_name in soup.findAll('tr', {'class': 'field-odd field'}):
+        print(item_name.string)
+        
+py_spider()
